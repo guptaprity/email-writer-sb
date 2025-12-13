e7f75c32c7f7cd227ad1bcdf510ce16c2d22a376
Prity Gupta <pritygupta310@gmail.com>
2025-12-13 16:18:52 +0530

    Changes in Email Request

--- src/main/java/com/email_writer/EmailRequest.java
+++ src/main/java/com/email_writer/EmailRequest.java
@@ -1,9 +1,13 @@
 package com.email_writer;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailRequest {
     private String emailContent;
     private String tone;
